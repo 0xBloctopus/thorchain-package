f5f0bc764ad09607d28b835af688023584052143
Til Jordan <til@bloctopus.io>
2025-10-01 13:10:18 +0000

    Filter supply array to only include denoms present in balances

--- src/genesis-generator/scripts/update_balances.sh
+++ src/genesis-generator/scripts/update_balances.sh
@@ -5,14 +5,21 @@ NEW_BALANCES_FILE="/tmp/new_balances.json"
 TEMP_FILE="/tmp/genesis_temp.json"
 
 jq --slurpfile new_balances "$NEW_BALANCES_FILE" '
+  ($new_balances[0] | map(.coins[].denom) | unique) as $balance_denoms |
+  
   .app_state.bank.balances = $new_balances[0] |
+  
+  .app_state.bank.supply = (.app_state.bank.supply | map(select(.denom as $d | $balance_denoms | contains([$d])))) |
+  
   .app_state.bank.supply |= map(
-    if .denom == "rune" then
-      .amount = ($new_balances[0] | map(.coins[] | select(.denom == "rune") | .amount | tonumber) | add | tostring)
-    else
-      .
-    end
+    .denom as $supply_denom |
+    .amount = (
+      $new_balances[0] 
+      | map(.coins[] | select(.denom == $supply_denom) | .amount | tonumber) 
+      | add 
+      | tostring
+    )
   )
 ' "$GENESIS_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$GENESIS_FILE"
 
-echo "Replaced balances and updated rune supply"
+echo "Replaced balances and filtered supply to match"
