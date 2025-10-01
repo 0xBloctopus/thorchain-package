a764324ad0b22901da822bfa0622c9642bbba7e3
Til Jordan <til@bloctopus.io>
2025-10-01 13:27:29 +0000

    Simplify supply handling using __RUNE_SUPPLY__ placeholder

--- src/genesis-generator/scripts/update_balances.sh
+++ src/genesis-generator/scripts/update_balances.sh
@@ -4,21 +4,10 @@ GENESIS_FILE="/tmp/genesis.json"
 NEW_BALANCES_FILE="/tmp/new_balances.json"
 TEMP_FILE="/tmp/genesis_temp.json"
 
-jq --slurpfile new_balances "$NEW_BALANCES_FILE" '
-  .app_state.bank.balances = $new_balances[0] |
-  
-  .app_state.bank.supply = [
-    ($new_balances[0] | map(.coins[].denom) | unique)[] as $denom |
-    {
-      denom: $denom,
-      amount: (
-        $new_balances[0] 
-        | map(.coins[] | select(.denom == $denom) | .amount | tonumber) 
-        | add 
-        | tostring
-      )
-    }
-  ]
-' "$GENESIS_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$GENESIS_FILE"
+jq --slurpfile new_balances "$NEW_BALANCES_FILE" '.app_state.bank.balances = $new_balances[0]' "$GENESIS_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$GENESIS_FILE"
 
-echo "Replaced balances and filtered supply to match balance denoms"
+RUNE_TOTAL=$(jq '[.app_state.bank.balances[].coins[] | select(.denom == "rune") | .amount | tonumber] | add | tostring' "$GENESIS_FILE")
+
+sed -i "s/\"__RUNE_SUPPLY__\"/\"$RUNE_TOTAL\"/" "$GENESIS_FILE"
+
+echo "Replaced balances and updated rune supply placeholder"
