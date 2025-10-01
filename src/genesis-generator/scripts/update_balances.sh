c815c6f4975aac72cd5a4792471112e0c4ce23aa
Til Jordan <til@bloctopus.io>
2025-10-01 13:38:11 +0000

    Append balances instead of replacing, add mainnet supply constant

--- src/genesis-generator/scripts/update_balances.sh
+++ src/genesis-generator/scripts/update_balances.sh
@@ -4,10 +4,14 @@ GENESIS_FILE="/tmp/genesis.json"
 NEW_BALANCES_FILE="/tmp/new_balances.json"
 TEMP_FILE="/tmp/genesis_temp.json"
 
-jq --slurpfile new_balances "$NEW_BALANCES_FILE" '.app_state.bank.balances = $new_balances[0]' "$GENESIS_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$GENESIS_FILE"
+MAINNET_RUNE_SUPPLY=42537131234170029
 
-RUNE_TOTAL=$(jq '[.app_state.bank.balances[].coins[] | select(.denom == "rune") | .amount | tonumber] | add | tostring' "$GENESIS_FILE")
+jq --slurpfile new_balances "$NEW_BALANCES_FILE" '.app_state.bank.balances += $new_balances[0]' "$GENESIS_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$GENESIS_FILE"
 
-sed -i "s/\"__RUNE_SUPPLY__\"/\"$RUNE_TOTAL\"/" "$GENESIS_FILE"
+OUR_RUNE_TOTAL=$(jq --slurpfile new_balances "$NEW_BALANCES_FILE" '$new_balances[0] | [.[].coins[] | select(.denom == "rune") | .amount | tonumber] | add' /dev/null)
 
-echo "Replaced balances and updated rune supply placeholder"
+TOTAL_RUNE_SUPPLY=$((MAINNET_RUNE_SUPPLY + OUR_RUNE_TOTAL))
+
+sed -i "s/\"__RUNE_SUPPLY__\"/\"$TOTAL_RUNE_SUPPLY\"/" "$GENESIS_FILE"
+
+echo "Appended balances and updated rune supply placeholder with total: $TOTAL_RUNE_SUPPLY"
