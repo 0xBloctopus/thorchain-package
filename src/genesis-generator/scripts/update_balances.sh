bd143096310a001754ee053a1cfb237a82679767
Til Jordan <til@bloctopus.io>
2025-10-01 13:19:00 +0000

    Revert supply filtering to preserve all mainnet denoms, only update rune amount

--- src/genesis-generator/scripts/update_balances.sh
+++ src/genesis-generator/scripts/update_balances.sh
@@ -5,21 +5,20 @@ NEW_BALANCES_FILE="/tmp/new_balances.json"
 TEMP_FILE="/tmp/genesis_temp.json"
 
 jq --slurpfile new_balances "$NEW_BALANCES_FILE" '
-  ($new_balances[0] | map(.coins[].denom) | unique) as $balance_denoms |
-  
   .app_state.bank.balances = $new_balances[0] |
   
-  .app_state.bank.supply = (.app_state.bank.supply | map(select(.denom as $d | $balance_denoms | contains([$d])))) |
-  
   .app_state.bank.supply |= map(
-    .denom as $supply_denom |
-    .amount = (
-      $new_balances[0] 
-      | map(.coins[] | select(.denom == $supply_denom) | .amount | tonumber) 
-      | add 
-      | tostring
-    )
+    if .denom == "rune" then
+      .amount = (
+        $new_balances[0] 
+        | map(.coins[] | select(.denom == "rune") | .amount | tonumber) 
+        | add 
+        | tostring
+      )
+    else
+      .
+    end
   )
 ' "$GENESIS_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$GENESIS_FILE"
 
-echo "Replaced balances and filtered supply to match"
+echo "Replaced balances and updated rune supply"
