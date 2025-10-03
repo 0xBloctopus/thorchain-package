9a56cea37601868d9b12d9009a8614edcc18049c
til <til@bloctopus.io>
2025-10-04 00:03:41 +0100

    Remove old scripts

--- src/genesis-generator/scripts/add_accounts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-GENESIS_FILE="/tmp/genesis.json"
-NEW_ACCOUNTS_FILE="/tmp/new_accounts.json"
-TEMP_FILE="/tmp/genesis_temp.json"
-
-jq --slurpfile new_accounts "$NEW_ACCOUNTS_FILE" '.app_state.auth.accounts += $new_accounts[0]' "$GENESIS_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$GENESIS_FILE"
-
-echo "Appended new accounts"
